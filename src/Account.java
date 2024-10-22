842aad2b3c26242864861427bd6eb49ff748f5a6
khel <obidos.michael.cautiber@gmail.com>
2024-10-22 20:49:52 +0800

    added ATM Machine functionality

--- src/Account.java
+++ src/Account.java
@@ -1,23 +1,27 @@
 public class Account {
     int balance;
 
-    public void Account(int balance){
+
+
+    public Account(int balance){
         this.balance = balance;
     }
 
     public void deposit (int amount){
         balance += amount;
         System.out.print("Successfully Deposited: " + amount);
+        System.out.println();
     }
 
     public void checkBalance (){
         System.out.print("Your current balance is: " + balance);
+        System.out.println();
     }
     public void withdraw (int amount) {
         if (balance >= amount){
             balance -= amount;
             System.out.print("Successfully withdraw: " + amount);
+            System.out.println();
         }
-
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,43 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        int decision;
         int amount;
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter amount");
-        amount = sc.nextInt();
+        Account michael = new Account(500);
+        do {
+            System.out.println("1: Check Balance");
+            System.out.println("2: Deposit");
+            System.out.println("3: Withdtraw");
+            System.out.println("4: Exit");
+
+            System.out.print("Enter transaction number: ");
+            decision = sc.nextInt();
+            switch (decision){
+                case 1:
+                    michael.checkBalance();
+                    break;
+                case 2:
+                    System.out.print("Enter Amount: ");
+                    amount = sc.nextInt();
+                    michael.deposit(amount);
+                    break;
+                case 3:
+                    System.out.print("Enter Amount: ");
+                    amount = sc.nextInt();
+                    michael.withdraw(amount);
+                    break;
+                case 4:
+                    System.out.println("Thank You!");
+                    break;
+            }
+        }
+        while(decision != 4);
+
+
+
 
 
     }
